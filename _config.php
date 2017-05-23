b2e2bea1fe17995f0c52ffff017f1781adcbabe0
mak001 <mak001001@gmail.com>
2017-05-23 06:27:48 -0500

    Moved some debug stuff to installer

--- _config.php
+++ _config.php
@@ -10,10 +10,4 @@ BreadcrumbNavigation::$stopAtPageType = false;
 BreadcrumbNavigation::$showHidden = false; 
 BreadcrumbNavigation::$homeURLSegment = 'home';
 
-if (!Director::isDev()) {
-    SS_Log::add_writer(new SS_LogFileWriter('../ss_error-warning.log', SS_Log::WARN, '<='));
-    SS_Log::add_writer(new SS_LogFileWriter('../ss_error.log', SS_Log::ERR));
-    SS_Log::add_writer(new SS_LogFileWriter('../ss_notices.log', SS_Log::NOTICE));
-}
-
 ?>
